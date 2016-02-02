4ca57e6aa7349fa2a1dd0d80fc277366015fc93f
Katirene <kat.traxler@gmail.com>
2016-02-01 18:10:13 -0600

    initial commit

---
+++ solo_script.js
@@ -0,0 +1,89 @@
+// ! ! !
+// Three Bugs
+
+var arrayAtticus = ["Atticus", "2405", "47000", 3];
+var arrayJem = ["Jem", "62347", "63500", 4];
+var arrayBoo = ["Boo", "11435", "54000", 3];
+var arrayScout = ["Scout", "6243", "74750", 5];
+
+var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
+
+//Create variables used to write to the DOM
+var newEl, newText, position;
+//Capture the position of insertion into the DOM
+var position = document.getElementById('content');
+
+//Loop the array, extracting each array and writing information to the DOM
+//Note that the information is not 'clean'
+//needed to specific the varible [i] index position as a parameter for the calculateSTI function on each loop through
+for(var i = 0; i < array.length; i++){
+	array[i] = calculateSTI(array[i]);
+ 	newEl = document.createElement('li');
+	newText = document.createTextNode(array[i][0] + ",    percent STI    " + array[i][1] + "     " + array[i][2] + "     " + array[i][3]);
+	newEl.appendChild(newText);
+	position.appendChild(newEl);
+}
+
+
+function calculateSTI(array1) {
+  var newArray = [];
+  newArray[0] = array1[0];
+
+  var employeeNumber = array1[1];
+  var baseSalary = array1[2];
+  var reviewScore = array1[3];
+
+  var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
+  if(bonus > 0.13){
+    bonus = 0.13;
+  }
+
+  newArray[1] = bonus;
+  //changed line 43 used math.round.
+  newArray[2] = Math.round(baseSalary * (1.0 + bonus), -1);
+  //needed to remove the * bonus from the result because I changed the input to newArray[2]
+  newArray[3] = Math.round(newArray[2]);
+  console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
+
+  return newArray;
+
+}
+//why were they subtracting 1 from the return basePercent? that was strange.
+function getBaseSTI(reviewScore) {
+  var basePercent;
+  switch(reviewScore){
+    case 1:
+      basePercent = 0;
+      break;
+    case 2:
+      basePercent = 0;
+      break;
+    case 3:
+      basePercent = 0.04;
+      break;
+    case 4:
+      basePercent = 0.06;
+      break;
+    case 5:
+      basePercent = 0.10;
+      break;
+  }
+  return basePercent; 
+}
+
+function getYearAdjustment(employeeNumber) {
+  var yearAdjustment = 0;
+  if(employeeNumber.length == 4){
+    yearAdjustment = 0.05;
+  }
+  return yearAdjustment;
+}
+
+function getIncomeAdjustment(baseSalary) {
+  var incomeAdjustment = 0;
+  baseSalary = parseInt(baseSalary);
+  if (baseSalary > 65000) {
+    incomeAdjustment = 0.01;
+  }
+  return incomeAdjustment;
+}
